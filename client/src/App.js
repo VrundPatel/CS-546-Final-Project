28d92b44f9a0765c75d3270c892274afe46d857c
Vrund Patel <vpatel2797@gmail.com>
2022-10-31 21:12:14 -0400

    Setup pages and install deps

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
