616f2097bcc4388299bb5ec401935a17964edc7f
Vrund Patel <vpatel2797@gmail.com>
2022-10-31 21:35:40 -0400

    Sign up page work

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,18 +5,18 @@ import './login.css';
 
 export default function Login() {
   return (
-    <div className='login-form-container'>
-      <Form className='login-form'>
-        <h2 style={{ 'textAlign': 'center'}}>Login to GottaGo</h2>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+    <div className='form-container'>
+      <Form className='custom-form'>
+        <h2 style={{ 'textAlign': 'center' }}>Login to GottaGo</h2>
+        <Form.Group className="mb-3" controlId="userEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3" controlId="userPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <p>Don't have an account? 
+        <p>Don't have an account?
           <Link to='/sign-up'>Sign Up</Link>
         </p>
         <Button variant="primary" type="submit">
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,7 +1,32 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from "react-router-dom";
+import './login.css';
+
 export default function SignUp() {
   return (
-    <>
-      <h1>Sign up Page</h1>
-    </>
-  )
+    <div className='form-container'>
+      <Form className='custom-form'>
+        <h2 style={{ 'textAlign': 'center' }}>SignUp for GottaGo</h2>
+        <Form.Group className="mb-3" controlId="userFullName">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter full name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="userEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="userPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <p>Already have an account?
+          <Link to='/login'>Login</Link>
+        </p>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
