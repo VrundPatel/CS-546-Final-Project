fe08ddfdeff54a8d62f72987b1445967a56e2372
Vrund Patel <vpatel2797@gmail.com>
2022-10-31 21:27:38 -0400

    Login page work

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 
-
 function App() {
   return (
     <BrowserRouter>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,28 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from "react-router-dom";
+import './login.css';
+
 export default function Login() {
   return (
-    <>
-      <h1>Login Page</h1>
-    </>
-  )
+    <div className='login-form-container'>
+      <Form className='login-form'>
+        <h2 style={{ 'textAlign': 'center'}}>Login to GottaGo</h2>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <p>Don't have an account? 
+          <Link to='/sign-up'>Sign Up</Link>
+        </p>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
