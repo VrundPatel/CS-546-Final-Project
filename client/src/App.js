4ed6a3132abed5d0b372b61850fa8c623849af1d
Vrund Patel <vpatel2797@gmail.com>
2022-11-01 08:45:24 -0400

    Updated to use new method

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -8,11 +8,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/">
-          <Redirect to="/login" />
-        </Route>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
