6629f05c58ec4ab9c20a37ddf8bd338d6e533107
Vrund Patel <vpatel2797@gmail.com>
2022-11-09 19:04:00 -0500

    Install axios and submit sign up data

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,25 +1,66 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from "react-router-dom";
 import './login.css';
+import axios from 'axios';
 
 export default function SignUp() {
+  const initialState = {
+    fullName: '',
+    email: '',
+    password: ''
+  };
+
+  const [formState, setFormState] = useState(initialState);
+  const { fullName, email, password } = formState;
+
+  const handleOnChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log('form state ', formState);
+    setFormState(initialState);
+    const { data } = await axios.post('http://localhost:9000/users', formState);
+  }
+
   return (
     <div className='form-container'>
-      <Form className='custom-form'>
+      <Form className='custom-form' onSubmit={handleSubmit}>
         <h2 style={{ 'textAlign': 'center' }}>SignUp for GottaGo</h2>
+
         <Form.Group className="mb-3" controlId="userFullName">
           <Form.Label>Full Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter full name" />
+          <Form.Control
+            onChange={handleOnChange}
+            value={fullName}
+            name='fullName'
+            type="text"
+            placeholder="Enter full name" />
         </Form.Group>
+
         <Form.Group className="mb-3" controlId="userEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            onChange={handleOnChange}
+            value={email}
+            name='email'
+            type="email"
+            placeholder="Enter email" />
         </Form.Group>
+
         <Form.Group className="mb-3" controlId="userPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            onChange={handleOnChange}
+            value={password}
+            name='password'
+            type="password"
+            placeholder="Password" />
         </Form.Group>
+
         <p>Already have an account?
           <Link to='/login'>Login</Link>
         </p>
