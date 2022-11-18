b56f249347f6031c577a00bbd91dc8294a3c8055
Vrund Patel <vpatel2797@gmail.com>
2022-11-17 21:03:34 -0500

    Format changes

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,40 +4,40 @@ const data = require('../data');
 const userData = data.users;
 
 router
-    .route('/')
-    .get(async (req, res) => {
-        const userList = await userData.getAllUsers();
-        res.json(userList);
-    })
-    .post(async (req, res) => {
-        // Creates a new user account
-        console.log('incoming request ', req.body);
-        const userCreated = await userData.createUser(req.body);
-        res.json({ created: true });
-    })
-    .delete(async (req, res) => {
-        // Deletes a user account. Probably not needed for the project?
-        res.send('DELETE request to http://localhost:3000/user');
-    });
+  .route('/')
+  .get(async (req, res) => {
+    const userList = await userData.getAllUsers();
+    res.json(userList);
+  })
+  .post(async (req, res) => {
+    // Creates a new user account
+    console.log('incoming request ', req.body);
+    const userCreated = await userData.createUser(req.body);
+    res.json({ created: true });
+  })
+  .delete(async (req, res) => {
+    // Deletes a user account. Probably not needed for the project?
+    res.send('DELETE request to http://localhost:3000/user');
+  });
 
 router
-    .route('/:userId')
-    .get(async (req, res) => {
-        try { // Error Checking
-            //TODO: ID validation
-        } catch (e) {
-            return res.status(400).json({ error: e });
-        }
-        try {
-            const user = await userData.getUserById(req.params.userId);
-            res.json(user);
-        } catch (e) {
-            res.status(404).json({ error: e });
-        }
-    })
-    .put(async (req, res) => {
-        //TODO: Updates an existing user with updated fields
-        res.send(`POST request to http://localhost:3000/user/${req.params.id}`);
-    })
+  .route('/:userId')
+  .get(async (req, res) => {
+    try { // Error Checking
+      //TODO: ID validation
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+    try {
+      const user = await userData.getUserById(req.params.userId);
+      res.json(user);
+    } catch (e) {
+      res.status(404).json({ error: e });
+    }
+  })
+  .put(async (req, res) => {
+    //TODO: Updates an existing user with updated fields
+    res.send(`POST request to http://localhost:3000/user/${req.params.id}`);
+  })
 
 module.exports = router;
