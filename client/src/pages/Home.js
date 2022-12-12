5c9e888533b78dbd679da7c82d457a0c0ce71229
Vrund Patel <vpatel2797@gmail.com>
2022-12-11 21:56:54 -0500

    fix geolocation search

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ import Layout from "./layout";
 
 export default function Restrooms() {
   const [restrooms, setRestrooms] = useState([]);
-	const [lat, setLat] = useState(null);
+  const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
   const [status, setStatus] = useState(null);
 
@@ -27,7 +27,7 @@ export default function Restrooms() {
     activeFilters: []
   };
 
-  
+
   const [genderNeutralCheckState, setGenderNeutralCheckState] = useState(false);
   const [adaCheckState, setAdaCheckState] = useState(false);
   const [stationCheckState, setStationCheckState] = useState(false);
@@ -44,28 +44,18 @@ export default function Restrooms() {
 
   const onSearchSubmit = async (event) => {
     event.preventDefault();
-		console.log("searching by term");
+    console.log("searching by term");
     try {
       const { data } = await axios.post(`http://localhost:9000/search`, {
         "searchRestrooms": searchTerm
       });
       setRestrooms(data);
-      // if (!!data) {
-      //   console.log('data returned ', data);
-      //   const foundRestroom = await compare(searchTerm, data.streetAddress);
-      //   if (foundRestroom) {
-      //     setRestrooms(data);
-      //   }
-      //   else {
-      //     alert(`Not Found! No such a restroom`);
-      //   }
-      // }
     } catch (e) {
       alert('Error')
     }
   }
 
-	const onLocationSubmit = async (event) => {
+  const onLocationSubmit = async (event) => {
     event.preventDefault();
     console.log("searching by device location");
     // Fetch device location
@@ -73,43 +63,43 @@ export default function Restrooms() {
       setStatus("Can't get location, try a different browser");
     } else {
       navigator.geolocation.getCurrentPosition(
-        (position) => {
-					console.log(position);
+        async (position) => {
+          console.log(position);
           setStatus(null);
           setLat(position.coords.latitude);
           setLng(position.coords.longitude);
+
+          try {
+            const { data } = await axios.get(`http://localhost:9000/search/${position.coords.latitude}/${position.coords.longitude}`);
+            setRestrooms(data);
+          } catch (e) {
+            alert('Error')
+          }
         },
         () => {
           setStatus("Unable to retrieve your location");
         }
       );
     }
-		// GET request with device coordinates
-		try {
-      const { data } = await axios.get(`http://localhost:9000/search/${lat}/${lng}`);
-      setRestrooms(data);
-    } catch (e) {
-      alert('Error')
-    }
-	}
+  }
 
   const activeFilters = [genderNeutralCheckState, adaCheckState, stationCheckState, buyCheckState, keyCheckState];
 
   const filteredRestrooms = restrooms.filter(filteredRestroom => {
     if (genderNeutralCheckState && !filteredRestroom.tags.includes("Gender-neutral")) {
-        return false;
+      return false;
     }
     if (adaCheckState && !filteredRestroom.tags.includes("ADA compliant")) {
-        return false;
+      return false;
     }
     if (stationCheckState && !filteredRestroom.tags.includes("Baby-changing")) {
-        return false;
+      return false;
     }
     if (buyCheckState && !filteredRestroom.tags.includes("Gotta buy something")) {
-        return false;
+      return false;
     }
     if (keyCheckState && !filteredRestroom.tags.includes("Ask for key")) {
-        return false;
+      return false;
     }
     return filteredRestroom;
   });
@@ -137,112 +127,112 @@ export default function Restrooms() {
               </Col>
             </Row>
           </Form>
-					<div className='location-button-container'>
-						<Button className = 'location-button' variant="primary" type="submit" onClick = {onLocationSubmit}>
-								Search Near Me
-						</Button>
-					</div>
+          <div className='location-button-container'>
+            <Button className='location-button' variant="primary" type="submit" onClick={onLocationSubmit}>
+              Search Near Me
+            </Button>
+          </div>
         </div>
 
         <div className='filter-container'>
           <Form className='filter-form' onSubmit={onSearchSubmit}>
-              <Form.Label>Filters</Form.Label>
-              <br/>
-                <ToggleButton
-                  id="gender-neutral-toggle-check"
-                  type="checkbox"
-                  variant="outline-primary"
-                  checked={genderNeutralCheckState}
-                  value="1"
-                  onChange={(e) => {
-                    setGenderNeutralCheckState(e.currentTarget.checked);
-                  }}
-                >
-                  Gender-neutral
-                </ToggleButton>
-                {" "}
-                <ToggleButton
-                  id="ada-toggle-check"
-                  type="checkbox"
-                  variant="outline-primary"
-                  checked={adaCheckState}
-                  value="1"
-                  onChange={(e) => {
-                    setAdaCheckState(e.currentTarget.checked);
-                  }}
-                >
-                  ADA compliant
-                </ToggleButton>
-                {" "}
-                <ToggleButton 
-                  id="station-toggle-check"
-                  type="checkbox"
-                  variant="outline-primary"
-                  checked={stationCheckState}
-                  value="1"
-                  onChange={(e) => {
-                    setStationCheckState(e.currentTarget.checked);
-                  }}
-                >
-                  Baby-Changing Station
-                </ToggleButton>
-                {" "}
-                <ToggleButton 
-                  id="buy-toggle-check"
-                  type="checkbox"
-                  variant="outline-primary"
-                  checked={buyCheckState}
-                  value="1"
-                  onChange={(e) => {
-                    setBuyCheckState(e.currentTarget.checked);
-                  }}
-                >
-                  Gotta Buy Something
-                </ToggleButton>
-                {" "}
-                <ToggleButton 
-                  id="key-toggle-check"
-                  type="checkbox"
-                  variant="outline-primary"
-                  checked={keyCheckState}
-                  value="1"
-                  onChange={(e) => {
-                    setKeyCheckState(e.currentTarget.checked);
-                  }}
-                >
-                  Ask For Key
-                </ToggleButton>
-                {" "}
-                <ToggleButton 
-                  id="reset-toggle-check"
-                  type="checkbox"
-                  variant="secondary"
-                  checked={resetCheckState}
-                  value="1"
-                  onChange={() => {
-                    setGenderNeutralCheckState(false);
-                    setAdaCheckState(false);
-                    setStationCheckState(false);
-                    setBuyCheckState(false);
-                    setKeyCheckState(false);
-                  }}
-                  disabled = {!activeFilters.filter(Boolean).length}
-                >
-                  Reset
-                </ToggleButton>
+            <Form.Label>Filters</Form.Label>
+            <br />
+            <ToggleButton
+              id="gender-neutral-toggle-check"
+              type="checkbox"
+              variant="outline-primary"
+              checked={genderNeutralCheckState}
+              value="1"
+              onChange={(e) => {
+                setGenderNeutralCheckState(e.currentTarget.checked);
+              }}
+            >
+              Gender-neutral
+            </ToggleButton>
+            {" "}
+            <ToggleButton
+              id="ada-toggle-check"
+              type="checkbox"
+              variant="outline-primary"
+              checked={adaCheckState}
+              value="1"
+              onChange={(e) => {
+                setAdaCheckState(e.currentTarget.checked);
+              }}
+            >
+              ADA compliant
+            </ToggleButton>
+            {" "}
+            <ToggleButton
+              id="station-toggle-check"
+              type="checkbox"
+              variant="outline-primary"
+              checked={stationCheckState}
+              value="1"
+              onChange={(e) => {
+                setStationCheckState(e.currentTarget.checked);
+              }}
+            >
+              Baby-Changing Station
+            </ToggleButton>
+            {" "}
+            <ToggleButton
+              id="buy-toggle-check"
+              type="checkbox"
+              variant="outline-primary"
+              checked={buyCheckState}
+              value="1"
+              onChange={(e) => {
+                setBuyCheckState(e.currentTarget.checked);
+              }}
+            >
+              Gotta Buy Something
+            </ToggleButton>
+            {" "}
+            <ToggleButton
+              id="key-toggle-check"
+              type="checkbox"
+              variant="outline-primary"
+              checked={keyCheckState}
+              value="1"
+              onChange={(e) => {
+                setKeyCheckState(e.currentTarget.checked);
+              }}
+            >
+              Ask For Key
+            </ToggleButton>
+            {" "}
+            <ToggleButton
+              id="reset-toggle-check"
+              type="checkbox"
+              variant="secondary"
+              checked={resetCheckState}
+              value="1"
+              onChange={() => {
+                setGenderNeutralCheckState(false);
+                setAdaCheckState(false);
+                setStationCheckState(false);
+                setBuyCheckState(false);
+                setKeyCheckState(false);
+              }}
+              disabled={!activeFilters.filter(Boolean).length}
+            >
+              Reset
+            </ToggleButton>
           </Form>
         </div>
 
         {/* Search Results with variable preprocesser filtering*/}
         <div>
-          <h2>{filteredRestrooms.length} Result{filteredRestrooms.length != 1 ? "s": ""}</h2>
+          <h2>{filteredRestrooms.length} Result{filteredRestrooms.length !== 1 ? "s" : ""}</h2>
           {filteredRestrooms.map((outputRestroom, index) => {
             return (
               <Card key={outputRestroom._id} style={{ width: "24rem" }}>
                 <Card.Body>
                   <h3>{index + 1}</h3>
                   <a href={"/restroom/" + outputRestroom._id}>{outputRestroom.streetAddress}</a>
-                  <br/>
+                  <br />
                   {outputRestroom.city}, {outputRestroom.state} {outputRestroom.zipCode}
                 </Card.Body>
               </Card>
