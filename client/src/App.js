e305e73e8faabd648654e5da506bc207787d26e2
Vrund Patel <vpatel2797@gmail.com>
2022-12-13 20:38:35 -0500

    prevent users from going to login and sign up when authenticated

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import GuardedRoute from "./pages/guarded-route";
-import Restrooms from "./pages/Home";
-import Login from "./pages/Login";
-import RestroomDetails from "./pages/RestroomDetails/RestroomDetails";
-import SignUp from "./pages/SignUp";
-import UserProfile from "./pages/UserProfile";
+import AppRoutes from "./pages/routes";
 
 export const AuthContext = React.createContext();
 axios.defaults.withCredentials = true;
@@ -16,28 +11,7 @@ axios.defaults.withCredentials = true;
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/home"
-          element={
-            <GuardedRoute>
-              <Restrooms />
-            </GuardedRoute>
-          }
-        />
-        <Route path="/restroom/:id" element={<RestroomDetails />} />
-        <Route
-          path="/user"
-          element={
-            <GuardedRoute>
-              <UserProfile />
-            </GuardedRoute>
-          }
-        />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
