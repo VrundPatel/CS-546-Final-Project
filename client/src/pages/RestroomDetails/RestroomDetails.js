45f75934fbf4ade587a81031d4727e2733300874
Vrund Patel <vpatel2797@gmail.com>
2022-12-15 17:58:03 -0500

    remove test data

--- client/src/pages/RestroomDetails/RestroomDetails.js
+++ client/src/pages/RestroomDetails/RestroomDetails.js
@@ -7,46 +7,6 @@ import WriteReview from "./WriteReview";
 import Layout from "../layout";
 import axios from "axios";
 
-let testData = {
-  _id: "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310",
-  streetAddress: "123 Main Street",
-  city: "NYC",
-  state: "NY",
-  zipCode: "12345",
-  overallRating: 2.0,
-  openingHours: "10AM",
-  closingHours: "10PM",
-  availability: "['gendered', 'gender-neutral']",
-  amenities: "['ADA', 'baby-changing station']",
-  reviews: [
-    {
-      _id: "7c7997a2-c0d2-4f8c-b27a-6a1d4b5b6acd",
-      reviewText: "Nice and clean place",
-      userId: "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310",
-      rating: 2.0,
-    },
-    {
-      _id: "7c7997a2-c0d2-4f8c-b27a-6a1d4b5b6ace",
-      reviewText:
-        "Nice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean placeNice and clean place",
-      userId: "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310",
-      rating: 2.0,
-    },
-  ],
-  reports: [
-    {
-      _id: "7e7997a2-c0d2-4f8c-b27a-6a1d4b5b6916",
-      value: "Down for maintenance",
-      userId: "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310",
-    },
-    {
-      _id: "7e7997a2-c0d2-4f8c-b27a-6a1d4b5b6917",
-      value: "Permanently closed",
-      userId: "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310",
-    },
-  ],
-};
-
 export default function RestroomDetails() {
   const [restroomData, setRestroomData] = useState(null);
   const [ready, setReady] = useState(false);
@@ -62,7 +22,7 @@ export default function RestroomDetails() {
       restroomId: id,
       value: reason
     });
-    setRestroomData(data);  
+    setRestroomData(data);
   }
 
 
@@ -111,7 +71,7 @@ export default function RestroomDetails() {
                               <Card key={item?._id}>
                                 <Card.Body>
                                   {`${item.value}`}
-                                  <Badge pill bg="primary" style={{float: 'right'}}>
+                                  <Badge pill bg="primary" style={{ float: 'right' }}>
                                     {item.reportedAt}
                                   </Badge>
                                 </Card.Body>
